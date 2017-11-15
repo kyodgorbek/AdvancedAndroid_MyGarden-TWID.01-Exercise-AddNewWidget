dd38aa903641197249577e186d8b3376b4975068
kyodgorbek <kyodgorbek@gmail.com>
2017-11-15 16:47:19 +0100

    I have update widget

--- app/src/main/java/com/example/android/mygarden/PlainWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlainWidgetProvider.java
@@ -1,10 +1,13 @@
 package com.example.android.mygarden;
 
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
-
+import com.example.android.mygarden.ui.*;
 /**
  * Implementation of App Widget functionality.
  */
@@ -13,10 +16,16 @@ public class PlainWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = "App Widget";
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
+
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
